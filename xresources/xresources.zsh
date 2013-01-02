5bd0d8a0989f27ff2588c487100e753ba0210d04
David Long <dave@davejlong.com>
2013-01-02 16:55:56 -0500

    Added Xresources

---
+++ xresources/xresources.zsh
@@ -0,0 +1,3 @@
+[[ -f ./baseXresources ]] && xrdb ./baseXresources
+# [[ -f ~/.Xdefaults/XresourceTomorrowNightEighties ]] && xrdb -merge ~/.Xdefaults/XresourceTomorrowNightEighties
+[[ -f ~/Xdefaults/solarized ]] && xrdb -merge ~/Xdefaults/solarized
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -72,7 +72,8 @@ directory_name(){
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
+# export PROMPT=$'\n$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n$(rb_prompt) in $(directory_name)\n› '
 export RPROMPT='$(date "+%x %T %Z")'
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
