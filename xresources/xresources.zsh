b9132362453ae2fae8b8ff828342b76869d77027
David Long <dave@davejlong.com>
2013-01-16 09:50:03 -0500

    Remapped tmux prefix to

--- xresources/xresources.zsh
+++
@@ -1,3 +0,0 @@
-[[ -f ./baseXresources ]] && xrdb ./baseXresources
-# [[ -f ~/.Xdefaults/XresourceTomorrowNightEighties ]] && xrdb -merge ~/.Xdefaults/XresourceTomorrowNightEighties
-[[ -f ~/Xdefaults/solarized ]] && xrdb -merge ~/Xdefaults/solarized
