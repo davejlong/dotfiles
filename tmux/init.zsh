eb99060bc858f1833d0e922a4b2f30606ec45e14
David Long <dave@davejlong.com>
2013-05-02 10:15:46 -0400

    Fixed Tmux init to support mac

--- tmux/init.zsh
+++ tmux/init.zsh
@@ -2,16 +2,18 @@
   #[ "$TMUX" = "" ] && tmux
 #fi
 
-if [ -f /usr/bin/tmux ] && [ -z $TMUX ]; then
-  if [ "$SSH_TTY" != "" -a "$TERM" -a "$TERM" != "screen" -a "$TERM" != "dumb" ]; then
-    pgrep tmux
-    # $? is the exit code of pgrep; 0 means there was a result (tmux is already running)
-    if [ $? -eq 0 ]; then
-      tmux -u attach -d
+if [ -f /usr/bin/tmux ]  || [ -f /usr/local/bin/tmux ]; then
+  if [ -z $TMUX ]; then
+    if [ "$SSH_TTY" != "" -a "$TERM" -a "$TERM" != "screen" -a "$TERM" != "dumb" ]; then
+      pgrep tmux
+      # $? is the exit code of pgrep; 0 means there was a result (tmux is already running)
+      if [ $? -eq 0 ]; then
+        tmux -u attach -d
+      else
+        tmux -u
+      fi
     else
-      tmux -u
+      tmux
     fi
-  else
-    tmux
   fi
 fi
