756179d60b81511523d3ab5d1361a4194846ae6a
David Long <dave@davejlong.com>
2013-05-07 16:01:06 -0400

    Removed RVM server

--- rvm/rvm.zsh
+++ rvm/rvm.zsh
@@ -1,5 +1,6 @@
 # Check for a local install
 [ -f "~/.rvm/scripts/rvm" ] && source ~/.rvm/scripts/rvm
 
+# TODO: Why does RVM brake when I have this?
 # Check for a multiuser install
-[ -f "/usr/local/rvm/scripts/rvm" ] && source /usr/local/rvm/scripts/rvm
+#[ -f "/usr/local/rvm/scripts/rvm" ] && source /usr/local/rvm/scripts/rvm
