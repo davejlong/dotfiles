06fe8031c02a881aa5b369ebe97a4b1466fb2c55
David Long <dave@davejlong.com>
2013-07-23 12:38:17 -0400

    Added new Prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,86 +2,32 @@ autoload colors && colors
 # cheers, @ehrenmurdick
 # http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
 
-git_branch() {
-  echo $(/usr/bin/git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-}
+# screenshot:
+#
+# https://raw.github.com/jm3/dotfiles/master/prompt.gif
 
-git_dirty() {
-  st=$(/usr/bin/git status 2>/dev/null | tail -n 1)
-  if [[ $st == "" ]]
-  then
-    echo ""
-  else
-    if [[ $st == "nothing to commit (working directory clean)" ]]
+awesome_davejlong_prompt() {
+  arrow="%{$fg[red]%}➜"
+  cwd="%{$fg[cyan]%}%B%3c%(#.#.)%b"
+
+  git_info() {
+    is_git_dirty() {
+      git status --porcelain | egrep "\sM|\sD|\?\?" &> /dev/null
+    }
+    branch=`git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* //' `
+    if [ ! -z "${branch}" ];
     then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-    else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
+      is_git_dirty
+      if [ $? -eq 0 ];
+      then
+        git_filth=" %{$fg[yellow]%}✗%{$reset_color%}"
+      fi
+      git_branch=" (%{$fg[red]%}${branch}%{$reset_color%})"
+      echo -n "${git_branch}${git_filth}"
     fi
-  fi
+  }
+
+  echo -n "${arrow} ${cwd}$( git_info) > "
 }
 
-git_prompt_info () {
- ref=$(/usr/bin/git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
- echo "${ref#refs/heads/}"
-}
-
-unpushed () {
-  /usr/bin/git cherry -v @{upstream} 2>/dev/null
-}
-
-need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
-    echo " "
-  else
-    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
-  fi
-}
-
-rb_prompt(){
-  if $(which rbenv &> /dev/null)
-  then
-	  echo "%{$fg_bold[yellow]%}$(rbenv version | awk '{print $1}')%{$reset_color%}"
-	else
-	  echo ""
-  fi
-}
-
-# This keeps the number of todos always available the right hand side of my
-# command line. I filter it to only count those tagged as "+next", so it's more
-# of a motivation to clear out the list.
-todo(){
-  if $(which todo.sh &> /dev/null)
-  then
-    num=$(echo $(todo.sh ls +next | wc -l))
-    let todos=num-2
-    if [ $todos != 0 ]
-    then
-      echo "$todos"
-    else
-      echo ""
-    fi
-  else
-    echo ""
-  fi
-}
-
-directory_name(){
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
-}
-
-# export PROMPT=$'\n$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)\n› '
-export PROMPT=$'\n$(rb_prompt) in $(directory_name)\n› '
-export RPROMPT='$(date "+%x %T %Z")'
-set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
-}
-
-precmd() {
-  title "zsh" "%m" "%55<...<%~"
-  set_prompt
-}
-
-PS1="$PS1"'$([ -n "$TMUX" ] && tmux setenv TMUXPWD_$(tmux display -p "#D" | tr -d %) "$PWD")'
+export PROMPT='$(awesome_davejlong_prompt)'
