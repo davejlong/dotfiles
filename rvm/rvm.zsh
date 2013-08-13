172fd5f62e5a2908f5c8890e076dd73e54df8645
David Long <dave@davejlong.com>
2013-08-13 16:06:44 -0400

    Removed RVM

--- rvm/rvm.zsh
+++
@@ -1,5 +0,0 @@
-# Check for a local install
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
-
-# Check for a multiuser install
-[[ -s "/usr/local/rvm/scripts/rvm" ]] && source "/usr/local/rvm/scripts/rvm"
