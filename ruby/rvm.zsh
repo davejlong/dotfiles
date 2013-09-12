7c07813bede5494230086e7862d2542313d5e55e
David Long <dave@davejlong.com>
2013-09-12 13:13:20 -0400

    Removed some extra stuff from dotfiles

--- ruby/rvm.zsh
+++ ruby/rvm.zsh
@@ -2,3 +2,5 @@ export RVM_DIR="$HOME/.rvm"
 [[ -s "$RVM_DIR/scripts/rvm" ]] && source "$RVM_DIR/scripts/rvm" # Load RVM into a shell session *as a function*
 
 [[ -s "$RVM_DIR/scripts/completion" ]] && source "$RVM_DIR/scripts/completion"
+
+PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
