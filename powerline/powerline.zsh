09a865b9b90539f7cd8630fbafccc21b999f7cdc
David Long <dave@davejlong.com>
2013-09-17 14:52:22 -0400

    Fixed powerline config for cross platform

--- powerline/powerline.zsh
+++ powerline/powerline.zsh
@@ -1,5 +1,9 @@
-export POWERLINE_HOME="$HOME/.local/lib/python2.7/site-packages/powerline"
-
-export PATH="$HOME/.local/bin:$PATH"
+if [ "$(uname -s)" == "Darwin" ]
+then
+  export POWERLINE_HOME="$(brew --prefix)/lib/python2.7/site-packages/powerline"
+else
+  export POWERLINE_HOME="$HOME/.local/lib/python2.7/site-packages/powerline"
+  export PATH="$HOME/.local/bin:$PATH"
+fi
 
 . $POWERLINE_HOME/bindings/zsh/powerline.zsh
