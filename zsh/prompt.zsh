83a8877a425277b2bca2b2381955be1012cfab6c
David Long <dave@davejlong.com>
2013-09-18 09:38:03 -0400

    Fixed zsh prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -6,11 +6,6 @@ autoload colors && colors
 #
 # https://raw.github.com/jm3/dotfiles/master/prompt.gif
 
-awesome_davejlong_prompt() {
-  arrow="%{$fg[red]%}›"
-  cwd="%{$fg[cyan]%}%B%3c%(#.#.)%b"
-
-  echo -n "${cwd} ${arrow}%{$reset_color%} "
-}
-
-export PROMPT='$(awesome_davejlong_prompt)'
+if [ -f $POWERLINE_HOME/bindings/zsh/powerline.zsh ]; then
+  . $POWERLINE_HOME/bindings/zsh/powerline.zsh
+fi
