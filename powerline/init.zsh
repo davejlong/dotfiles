2af2f3631700626521d133b846fbd76189d7bc8c
David Long <dave@davejlong.com>
2013-10-23 10:38:51 -0400

    Removed powerline

--- powerline/init.zsh
+++
@@ -1,3 +0,0 @@
-if [ -f $POWERLINE_HOME/bindings/zsh/powerline.zsh ]; then
-  . $POWERLINE_HOME/bindings/zsh/powerline.zsh
-fi
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -6,6 +6,28 @@ autoload colors && colors
 #
 # https://raw.github.com/jm3/dotfiles/master/prompt.gif
 
-if [ -f $POWERLINE_HOME/bindings/zsh/powerline.zsh ]; then
-  . $POWERLINE_HOME/bindings/zsh/powerline.zsh
-fi
+awesome_davejlong_prompt() {
+  arrow="%{$fg[red]%}›"
+  cwd="%{$fg[cyan]%}%B%3c%(#.#.)%b"
+
+  git_info() {
+    is_git_dirty() {
+      git status --porcelain | egrep "\sM|\sD|\?\?" &> /dev/null
+    }
+    branch=`git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* //' `
+    if [ ! -z "${branch}" ];
+    then
+      is_git_dirty
+      if [ $? -eq 0 ];
+      then
+        git_filth=" %{$fg[yellow]%}✗%{$reset_color%}"
+      fi
+      git_branch=" (%{$fg[red]%}${branch}%{$reset_color%})"
+      echo -n "${git_branch}${git_filth}"
+    fi
+  }
+
+  echo -n "${cwd}$( git_info) ${arrow}%{$reset_color%} "
+}
+
+export PROMPT='$(awesome_davejlong_prompt)'
