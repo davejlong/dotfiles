4911441cf25047747a743a8445670f050fd68ff5
David Long <dave@davejlong.com>
2013-11-19 10:28:06 -0500

    Updated tmux to stop kicking people off

--- tmux/init.zsh
+++ tmux/init.zsh
@@ -8,7 +8,7 @@ if [ -f /usr/bin/tmux ]  || [ -f /usr/local/bin/tmux ]; then
       pgrep tmux
       # $? is the exit code of pgrep; 0 means there was a result (tmux is already running)
       if [ $? -eq 0 ]; then
-        tmux -u attach -d
+        tmux -u attach
       else
         tmux -u
       fi
