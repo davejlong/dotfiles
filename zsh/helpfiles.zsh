3763503b603909c42a5477a52c5f39fa37753d97
David Long <dave@davejlong.com>
2014-01-02 14:47:24 -0500

    Added condition for help files

--- zsh/helpfiles.zsh
+++ zsh/helpfiles.zsh
@@ -1,3 +1,6 @@
-unalias run-help
-autoload run-help
-HELPDIR=/usr/local/share/zsh/helpfiles
+if [[ -d /usr/local/share/zsh/helpfiles ]];
+then
+  unalias run-help
+  autoload run-help
+  HELPDIR=/usr/local/share/zsh/helpfiles
+fi
