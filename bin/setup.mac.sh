f58fb0ef11435683c31eec3bd79bb81f2897a874
Dave Long <dlong@cagedata.com>
2014-09-05 16:54:52 -0400

    New configuration for setting up mac

--- bin/setup.mac.sh
+++ bin/setup.mac.sh
@@ -5,12 +5,15 @@ if ! [[ -x /usr/local/bin/brew ]]; then
   brew install caskroom/cask/brew-cask
 
   brew tap thoughtbot/formulae
+  brew tap homebrew/nginx
 
   brew install rcm
 
-  brew install ack ansible w3m tmux redis postgres hub gitsh zsh wget
-  # TODO Add install for customized packages: Mutt, Vim, MacVim, Nginx
+  brew install ack ansible w3m tmux redis postgres hub gitsh zsh wget ssh-copy-id
   brew install qlcolorcode qlstephen qlmarkdown quicklook-json quicklook-csv qlprettypatch
+
+  brew install macvim --with-python --with-lua --override-system-vim --with-ruby
+  brew install nginx-full --with-realip --with-geoip --with-gzip-static --with-image-filter --with-pcre-jit --with-sub --with-status
 fi
 
 rcup # Install the rest of dotfiles
